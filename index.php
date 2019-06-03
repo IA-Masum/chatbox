69d3ce9043ec1f2e93cb6865e418ea64ec8f81fe
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2019-06-03 16:02:11 +0600

    basic setup completed

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Chat Box</title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- My Custom CSS -->
+  <link rel="stylesheet" href="css/my-custom.css">
+</head>
+<body>
+
+<h1>Welcome To chatbox!!</h1>
+  
+</body>
+
+<!-- JQuery -->
+<script src="js/jquery-3.4.0.js"></script>
+<!-- Popper js -->
+<script src="js/popper.js"></script>
+<!-- Bootsrtap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- FontAwesome -->
+<script src="https://kit.fontawesome.com/e630ea84f8.js"></script>
+</html>
