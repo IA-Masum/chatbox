f65e80b56e9a5818f64ed8f104816b550fa6155d
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2019-06-14 22:02:00 +0600

    Friends & Requests page design completed

--- friends.php
+++ friends.php
@@ -1,3 +1,200 @@
-<?php 
-  echo "friends";
-?>
+<div class="container pb-4">
+  <div class="row">
+
+    <div class="col-sm-6 border pt-2">
+      <h5 class="text-center">Your Friends</h5>
+      <hr>
+      <div style="height: 70vh; overflow: auto">
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-primary">Message</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 border pt-2">
+      <h5 class="text-center">Resently Connected</h5>
+      <hr>
+      <div style="height: 70vh; overflow: auto">
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Reply</a>
+            <a href="#" class="btn btn-sm px-3 btn-secondary">Profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
--- requests.php
+++ requests.php
@@ -1,3 +1,328 @@
-<?php 
-  echo "Requests";
-?>
+<div class="container pb-4">
+  <div class="row">
+
+    <div class="col-sm-6 border pt-2">
+      <h5 class="text-center">Sent Requests</h5>
+      <hr>
+      <div style="height: 70vh; overflow: auto">
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-warning">Cancel</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 border pt-2">
+      <h5 class="text-center">Received Requests</h5>
+      <hr>
+      <div style="height: 70vh; overflow: auto">
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+        <div class="border rounded border-danger p-2 my-1 d-flex justify-content-around">
+          <div class="mx-3 mt-2">
+            <img class="rounded-circle" width="30px" src="user.jpg">
+          </div>
+          <div class="mx-3">
+            <h6>Imran Ali Masum</h6>
+            <small>iamasum@gmail.com</small>
+          </div>
+          <div class="mx-3 mt-2">
+            <a href="#" class="btn btn-sm px-3 btn-success">Accept</a>
+            <a href="#" class="btn btn-sm px-3 btn-danger">Reject</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
